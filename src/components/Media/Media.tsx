b63f8793c1b05b18e45f57924d0d44a08330b77d
potsnpans2021 <>
2021-03-07 17:29:42 -0500

    blurred images

--- src/components/Media/Media.tsx
+++ src/components/Media/Media.tsx
@@ -1,15 +1,25 @@
-import { FC } from "react"
+import { FC, useState } from "react"
 import { Format } from "../../models/Format"
 import { getUrlMediaFormat } from "../../utils/formatUtils"
+import BlurOffIcon from '@material-ui/icons/BlurOff';
+import './Media.scss'
 
 export interface MediaProps {
     src: string;
     alt: string;
 }
+
+const shouldBlurImage = (src: string) => /blur/.test(src)
 export const Media: FC<MediaProps> = ({src, alt}) => {
+    const [blurred, setBlurred] = useState(shouldBlurImage(src))
     return <>
-        {/* for now just do this because react uses datauri for small images */}
-        {getUrlMediaFormat(src) !== Format.video && <img src={src} alt={alt} />}
+        {/* for now just do this because cra uses datauri for small images */}
+        {getUrlMediaFormat(src) !== Format.video &&
+            <div className='media-image-container'>
+                {blurred && <BlurOffIcon onClick={() => setBlurred(false)} fontSize='large' />}
+                <img src={src} alt={alt} className={blurred ? 'blurred' : ''}/>
+            </div>
+        }
         {getUrlMediaFormat(src) === Format.video && <video controls><source src={src} /></video>}
     </>
 }
