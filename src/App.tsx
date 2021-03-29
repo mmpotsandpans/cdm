3058ce3f68f588fa1158b72dd1ba3c630b30d422
potsnpans2021 <>
2021-03-28 23:51:50 -0400

    road map update

--- src/App.tsx
+++ src/App.tsx
@@ -30,6 +30,8 @@ const pageNames: any = {
   '/memorial': 'Memorial',
   '/form': 'Form',
   '/resources': 'Resources',
+  '/about': 'About',
+  '/graphs': 'Graphs',
   '/': ''
 }
 
--- src/views/People/People.tsx
+++ src/views/People/People.tsx
@@ -305,11 +305,10 @@ export const PeopleBreakdown: FC<{}> = () => {
             open={snackbarOpen}
             message={
                 <div style={{fontSize: '0.8em', textAlign: 'left'}}>
-                    <p>{t`အချက်အလက်များ၊ ရုပ်ပုံများကို အွန်လိုင်းမှ ရယူကာ အလျင်မှီသလို ထည့်သွင်းပြင်ဆင်ထားပါသည်။ မူပိုင်ခွင့်သည် ပိုင်ရှင်များနှင့်သာ သက်ဆိုင်ပါသည်။`}</p>
                     <p>{t`သတင်းမှားယွင်းမှုရှိပါက အတတ်နိုင်ဆုံး ပြင်ဆင်ပေးသွားပါမယ်။ သတင်းပေးပို့၊ ဖြည့်စွက်လိုပါက Controlsကိုနှိပ်ပြီး Formဖြည့်ပေးနိုင်ပါတယ်။`}</p>
                     <p>{t`သတင်းပေးရှာဖွေမှုလွယ်ကူစေရန် ကျဆုံးသွားသူများအတွက် #mmFallenHeroes နဲ့ ပျောက်ဆုံးနေသူများအတွက် #mmMissingHeroes ဟုအသုံးပြုပေးကြပါ။`}</p>
                     <p>{t`ကျန်ရစ်သူများကို ကူညီလိုပါက`} <NavLink to="/resources" style={{color: 'yellowgreen'}}>link</NavLink></p>
-                    <p style={{textAlign: 'center'}}>{t`အာဇာနည်များအား ဝမ်းနည်းလှစွာဖြင့် ဂုဏ်ပြုမှတ်တမ်းတင်အပ်ပါသည်။`} <NavLink to='/about' style={{color: 'yellow'}}>About</NavLink></p>
+                    <p style={{textAlign: 'center'}}>{t`အာဇာနည်များအား ဝမ်းနည်းလှစွာဖြင့် ဂုဏ်ပြုမှတ်တမ်းတင်အပ်ပါသည်။`} <NavLink to='/about' style={{color: 'yellow', fontSize: '1.1rem'}}>Our road map</NavLink></p>
                 </div>
             }
             autoHideDuration={20000}
