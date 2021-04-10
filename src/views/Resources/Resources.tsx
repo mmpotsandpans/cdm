d16018a577a23ef706f641c9de197303c0d041a0
potsnpans2021 <>
2021-04-09 22:32:17 -0400

    resources breakdown

--- src/views/Resources/Resources.tsx
+++ src/views/Resources/Resources.tsx
@@ -5,28 +5,40 @@ import {t} from 'ttag'
 import './Resources.scss'
 
 const resources = {
-    'Fallen Heroes Fund Myanmar': 'https://www.facebook.com/fhfmyanmar',
-    'I Support Myanmar': 'https://www.isupportmyanmar.com/',
-    'Phan Foundation': 'https://phanfoundation.org/appeal-for-idps/',
-    'Mutal Aid Myanmar': 'https://www.mutualaidmyanmar.org/',
-    'Memoirs of the Fallen': 'https://memoirsofthefallen.com',
-    'Nway Oo Heroes': 'https://nwayooheroes.org',
-    'Ku Nyi': 'https://www.facebook.com/Ku-Nyi-100280548827059/',
-    'Myanmar Fallen Heroes': 'https://myanmarfallenheroes.com',
-    'Heroes of Spring Revolution 2021': 'https://heroesofspringrevolution2021.com/',
-    'Honor Heroes Myanmar': 'https://www.facebook.com/pg/HonorHeroesMyanmar/',
-    'Lu Pyout': 'https://lupyout.com',
-    'Nway Oo Revolution': 'https://nwayoorevolution.com',
-    'How Many Dead Bodies': 'https://www.facebook.com/How-many-dead-bodies-112813997526188/',
-    'Myanmar Spring Revolution Fallen Stars': 'https://bit.ly/myanmarspring22222',
-    'Myanmar Freedom Memorial': 'https://myanmarfreedommemorial.info/'
+    'international': {
+        'Mutal Aid Myanmar': 'https://www.mutualaidmyanmar.org/',
+        'I Support Myanmar': 'https://www.isupportmyanmar.com/',
+        'Phan Foundation': 'https://phanfoundation.org/appeal-for-idps/',
+        'Myanmar Fallen Heroes': 'https://myanmarfallenheroes.com',
+    },
+    'local': {
+        'Fallen Heroes Fund Myanmar': 'https://www.facebook.com/fhfmyanmar',
+        'Memoirs of the Fallen': 'https://memoirsofthefallen.com',
+        'Nway Oo Heroes': 'https://nwayooheroes.org',
+        'Heroes of Spring Revolution 2021': 'https://heroesofspringrevolution2021.com/',
+        'Ku Nyi': 'https://www.facebook.com/Ku-Nyi-100280548827059/',
+        'Honor Heroes Myanmar': 'https://www.facebook.com/pg/HonorHeroesMyanmar/',
+        'Lu Pyout': 'https://lupyout.com',
+        'Nway Oo Revolution': 'https://nwayoorevolution.com',
+        'How Many Dead Bodies': 'https://www.facebook.com/How-many-dead-bodies-112813997526188/',
+        'Myanmar Spring Revolution Fallen Stars': 'https://bit.ly/myanmarspring22222',
+        'Myanmar Freedom Memorial': 'https://myanmarfreedommemorial.info/'
+    },
 }
 export const Resources: FC<{}> = () => {
     return (
         <div className='Resources'>
             <h1>{t`အကူအညီပေးနိုင်သော၊ မှတ်တမ်းတင်ထားသော နေရာများ (ဒီဆိုက်နှင့် မသက်ဆိုင်)`}</h1>
+            <h2>International</h2>
             <List>
-                {Object.entries(resources).map(([name, link]) => (
+                {Object.entries(resources.international).map(([name, link]) => (
+                    // eslint-disable-next-line react/jsx-no-target-blank
+                    <ListItem key={name}><a href={link} target='_blank' rel='noopener'>{name}</a></ListItem>
+                ))}
+            </List>
+            <h2>Local</h2>
+            <List>
+                {Object.entries(resources.local).map(([name, link]) => (
                     // eslint-disable-next-line react/jsx-no-target-blank
                     <ListItem key={name}><a href={link} target='_blank' rel='noopener'>{name}</a></ListItem>
                 ))}
