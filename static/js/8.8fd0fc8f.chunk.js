a8f6a50025e39572f80f9b00088c0749b760b575
potsnpans2021 <>
2021-04-19 21:41:13 -0400

    Updates

---
+++ static/js/8.8fd0fc8f.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpcdm=this.webpackJsonpcdm||[]).push([[8],{1991:function(t,e,c){"use strict";c.r(e),c.d(e,"Redirect",(function(){return r}));var n=c(0),i=c(1),r=function(t){var e=t.url;return Object(n.useLayoutEffect)((function(){e&&(document.location.href=e)}),[e]),Object(i.jsx)("div",{style:{padding:"2em"},children:"redirecting..."})};e.default=r}}]);
+//# sourceMappingURL=8.8fd0fc8f.chunk.js.map
