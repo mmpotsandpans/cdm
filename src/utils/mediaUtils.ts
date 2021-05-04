d77c88a1035e29de9a0db09b2b90d85f17a419fc
potsnpans2021 <>
2021-05-03 21:08:00 -0400

    jfif support

--- src/utils/mediaUtils.ts
+++ src/utils/mediaUtils.ts
@@ -1,7 +1,7 @@
 import { Format } from '../models/Format'
 
 export const getMediaFormatFromUrl = (url: string) => {
-    if (/\.(png|jpe?g|svg|gif)$/i.test(url)) {
+    if (/\.(png|jpe?g|jfif|svg|gif)$/i.test(url)) {
         return Format.image
     } else if (/\.(mov|avi|flv|wmv|3gp|mp4|mpe?g)$/i.test(url)) {
         return Format.video
